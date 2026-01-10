9efff79781fb621516500e1c03188c3d6dc594cc
BimaNtx <binarybeast029@gmail.com>
2026-01-10 22:36:14 +0700

    Latihan loop tipis-tipis

---
+++ Loop/loop.js
@@ -0,0 +1,15 @@
+// for (let i = 1; i <= 10; i++){ //initial; condition; incr/decr
+//   console.log(i);
+// }
+
+// for (let i = 0; i <= 20; i += 2){ // menampilkan angka genap
+//   console.log(i);
+// }
+
+// for (let i = 20; i >= 0; i--){ 
+//   console.log(i);
+// }
+
+for (let i = 10; i <= 10000; i *= 10){ 
+  console.log(i);
+}
