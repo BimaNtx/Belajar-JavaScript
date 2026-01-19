4129aa911a1986028db3500a832b839f3679a015
BimaNtx <binarybeast029@gmail.com>
2026-01-19 21:32:40 +0700

    break dalam for

--- Loop/whileLoop.js
+++ Loop/whileLoop.js
@@ -14,9 +14,15 @@
 // alert ('welcome back admin');
 
 //! break untuk memberhentikan perulangan
-let input = prompt("Halo, Lagi ngapain?");
-while (true) {
-  input = prompt(input);
-  if (input.toLowerCase() === 'stop') break;
+// let input = prompt("Halo, Lagi ngapain?");
+// while (true) {
+//   input = prompt(input);
+//   if (input.toLowerCase() === 'stop') break;
+// }
+// alert('Siap');
+
+//! break dalam for 
+for (let i = 0; i < 1000; i++) {
+  console.log(i);
+  if (i === 100) break;
 }
-alert('Siap');
