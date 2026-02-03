21f6569292c79b550aac116141fd7209f6a08a3c
BimaNtx <binarybeast029@gmail.com>
2026-02-03 21:11:58 +0700

    Belajar Function. Argumen & parameter serta multiplenya

---
+++ Function/function.js
@@ -0,0 +1,22 @@
+// let lemparDadu = Math.floor(Math.random() * 6) + 1;
+// console.log(lemparDadu);
+
+function lemparDadu() {
+  console.log(Math.floor(Math.random() * 6) + 1); //lebih singkat dan dapat digunakan berkali kali
+}
+
+function selamatSiang(name) { //(name) sebagai parameter
+  console.log(`Hello, ${name}. Selamat Siang Guys`);
+}
+
+selamatSiang('Bima'); //'Bima' sebagai argument
+
+// parameter/argument = input
+
+//! multiple arguments & parameters
+function jumlahkan(a, b) {
+  const total = a + b;
+  console.log(total);
+}
+
+jumlahkan(10, 1000) //jika memasukkan 3 argument, yang dihitung hanya sesuai dengan jumlah parameter
